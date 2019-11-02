e92f5bc4e1e4e986b4e132a8a0b15555704c8c8c
Alison Vermeil <alison.minton@gmail.com>
2019-11-02 11:35:13 -0600

    Add methods for generate_number and set_keys

--- lib/key.rb
+++ lib/key.rb
@@ -1,10 +1,23 @@
 class Key
   attr_reader :a_key, :b_key, :c_key, :d_key
 
-  def initialize(number)
-    @a_key = number[0,1]
-    @b_key = number[1][2]
-    @c_key = number[2][3]
-    @d_key = number[3][4]
+  def initialize
+    @a_key = nil
+    @b_key = nil
+    @c_key = nil
+    @d_key = nil
   end
+
+  def generate_number
+    number = rand(10000..100000).to_s
+  end
+
+  def set_keys
+    number = generate_number
+    @a_key = number[0..1]
+    @b_key = number[1..2]
+    @c_key = number[2..3]
+    @d_key = number[3..4]
+  end
+
 end
