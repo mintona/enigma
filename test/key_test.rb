cc1e0540f7f8a1b6750f4a77eca3374e7312aecc
Alison Vermeil <alison.minton@gmail.com>
2019-11-02 11:34:51 -0600

    Complete tests for generate_random_number and set_keys

--- test/key_test.rb
+++ test/key_test.rb
@@ -4,9 +4,7 @@ require_relative '../lib/key'
 class KeyTest < Minitest::Test
 
   def setup
-    #it will need an argument of a 5 digit number
-    @random_number = 12345
-    @key = Key.new(@random_number)
+    @key = Key.new
   end
 
   def test_it_exists
@@ -14,14 +12,39 @@ class KeyTest < Minitest::Test
   end
 
   def test_it_has_attributes
-    assert_equal 12, @key.a_key
-    assert_equal 23, @key.b_key
-    assert_equal 34, @key.c_key
-    assert_equal 45, @key.d_key
+    assert_nil @key.a_key
+    assert_nil @key.b_key
+    assert_nil @key.c_key
+    assert_nil @key.d_key
   end
 
-  def test_it_has_5_digits
-    skip
-    assert_equal true, @key.five_digits?
+  def test_it_can_generate_random_five_digit_number
+    number1 = @key.generate_number
+
+    assert_equal 5, number1.length
+
+    number2 = @key.generate_number
+
+    assert_equal 5, number2.length
+
+    number3 = @key.generate_number
+
+    assert_equal 5, number3.length
+
+    expected = number1 != number2 && number2 != number3 && number1 != number3
+    #should this be tested more? this will be a problem if it ever generates the same number twice
+    #seems super unlikely
+    assert_equal true, expected
   end
+
+  def test_it_can_set_the_keys
+    @key.expects(:generate_number).returns("12345")
+    @key.set_keys
+
+    assert_equal '12', @key.a_key
+    assert_equal '23', @key.b_key
+    assert_equal '34', @key.c_key
+    assert_equal '45', @key.d_key
+  end
+
 end
