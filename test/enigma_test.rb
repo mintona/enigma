e1ca9561d50d18ec7317d6e167b8ea87d3a54475
Alison Vermeil <alison.minton@gmail.com>
2019-11-03 14:02:24 -0700

    Build on tests for creating shifts and encrypting a message WIP

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -18,19 +18,32 @@ class EnigmaTest < Minitest::Test
   def test_it_has_a_key_and_date
     assert_nil @enigma.key
     assert_nil @enigma.date
+    assert_nil @enigma.shift
   end
 
   def test_it_can_get_shift_with_key_and_date
 
+    @enigma.create_shift("02715", "040895")
 
+    assert_equal 3, @enigma.shift.a_shift
+    assert_equal 27, @enigma.shift.b_shift
+    assert_equal 73, @enigma.shift.c_shift
+    assert_equal 20, @enigma.shift.d_shift
   end
 
+
+
   def test_it_can_shift_characters
-    assert_equal '', @enigma.shift("Lets TRY this AGAIN!!")
+    skip
+    assert_equal '', @enigma.shift_message("Lets TRY this AGAIN!!")
   end
 
   def test_it_can_encrypt_a_message
-    skip
+    @enigma.expects(:shift.a_shift).returns(3)
+    @enigma.expects(:shift.b_shift).returns(27)
+    @enigma.expects(:shift.c_shift).returns(73)
+    @enigma.expects(:shift.d_shift).returns(20)
+    require "pry"; binding.pry
     expected = {
                 encryption: "keder ohulw",
                 key: "02715",
