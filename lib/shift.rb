205a762acb69aad70e5fb96831e9dac08e63db28
Alison Vermeil <alison.minton@gmail.com>
2019-11-03 15:03:48 -0700

    Refactor shift to not use key or offset objects and change create_shift method accordingly

--- lib/shift.rb
+++ lib/shift.rb
@@ -2,6 +2,7 @@ class Shift
   attr_reader :key, :offset, :a_shift, :b_shift, :c_shift, :d_shift
 
   def initialize(key, offset)
+    # def initialize(key = Key.generate_number, offset = Offset.generate_date)
     @key = key
     @offset = offset
     @a_shift = nil
@@ -11,11 +12,25 @@ class Shift
   end
 
   def create_shift
-    @a_shift = @key.a_key.to_i + @offset.a_offset.to_i
-    @b_shift = @key.b_key.to_i + @offset.b_offset.to_i
-    @c_shift = @key.c_key.to_i + @offset.c_offset.to_i
-    @d_shift = @key.d_key.to_i + @offset.d_offset.to_i
+    @a_shift = @key[0..1].to_i + @offset[0].to_i
+    @b_shift = @key[1..2].to_i + @offset[1].to_i
+    @c_shift = @key[2..3].to_i + @offset[2].to_i
+    @d_shift = @key[3..4].to_i + @offset[3].to_i
+    # keys =  {
+    #         a_key => @key[0..1],
+    #         b_key => @key[1..2],
+    #         c_key => @key[2..3],
+    #         d_key => @key[3..4]
+    #         }
+    # @offset
+
   end
+  # def create_shift
+  #   @a_shift = @key.a_key.to_i + @offset.a_offset.to_i
+  #   @b_shift = @key.b_key.to_i + @offset.b_offset.to_i
+  #   @c_shift = @key.c_key.to_i + @offset.c_offset.to_i
+  #   @d_shift = @key.d_key.to_i + @offset.d_offset.to_i
+  # end
 
 
 end
