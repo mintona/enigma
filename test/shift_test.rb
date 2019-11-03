8562e48102ec93da0d8fb800ecf5748e82d76501
Alison Vermeil <alison.minton@gmail.com>
2019-11-03 15:02:58 -0700

    Refactor shift_tests to not need key or offset objects and include test for creating shift without knowing date or key

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -6,24 +6,35 @@ require_relative '../lib/shift'
 class ShiftTest < Minitest::Test
 
   def setup
-    @key = Key.new
-    @key.expects(:generate_number).returns("12345")
-    @key.set_keys
+    # @key = Key.new
+    # @key.expects(:generate_number).returns("12345")
+    # @key.set_keys
+    #
+    # date = Date.new(2019,11,2)
+    # @offset = Offset.new(date)
+    # @offset.set_offsets
+    #
+    # @shift = Shift.new(@key, @offset)
 
-    date = Date.new(2019,11,2)
-    @offset = Offset.new(date)
-    @offset.set_offsets
-
-    @shift = Shift.new(@key, @offset)
+    key = "02715"
+    offset = "1025"
+    @shift = Shift.new(key, offset)
   end
 
   def test_it_exists
-    assert_instance_of Shift, @shift
+    shift = Shift.new("12345", "021119")
+    assert_instance_of Shift, shift
   end
 
+  # use this test if this is needed later
+  # def test_it_can_be_created_without_given_key_and_offset
+  #   shift = Shift.new
+  #   assert_instance_of Shift, shift
+  # end
+
   def test_it_has_attributes
-    assert_equal @key, @shift.key
-    assert_equal @offset, @shift.offset
+    assert_equal "02715", @shift.key
+    assert_equal "1025", @shift.offset
     assert_nil @shift.a_shift
     assert_nil @shift.b_shift
     assert_nil @shift.c_shift
@@ -33,9 +44,18 @@ class ShiftTest < Minitest::Test
   def test_it_can_create_shift
     @shift.create_shift
 
-    assert_equal 14, @shift.a_shift
-    assert_equal 24, @shift.b_shift
-    assert_equal 40, @shift.c_shift
-    assert_equal 46, @shift.d_shift
+    assert_equal 3, @shift.a_shift
+    assert_equal 27, @shift.b_shift
+    assert_equal 73, @shift.c_shift
+    assert_equal 20, @shift.d_shift
   end
+#
+#   def test_it_can_create_shift
+#     @shift.create_shift
+#
+#     assert_equal 14, @shift.a_shift
+#     assert_equal 24, @shift.b_shift
+#     assert_equal 40, @shift.c_shift
+#     assert_equal 46, @shift.d_shift
+#   end
 end
