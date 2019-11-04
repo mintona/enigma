ecd9ce3e9e6d5392b4c9b6361211e0e52f182c73
Alison Vermeil <alison.minton@gmail.com>
2019-11-04 12:58:37 -0700

    Change variables from offset to date and remove comments

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -6,19 +6,9 @@ require_relative '../lib/shift'
 class ShiftTest < Minitest::Test
 
   def setup
-    # @key = Key.new
-    # @key.expects(:generate_number).returns("12345")
-    # @key.set_keys
-    #
-    # date = Date.new(2019,11,2)
-    # @offset = Offset.new(date)
-    # @offset.set_offsets
-    #
-    # @shift = Shift.new(@key, @offset)
-
     key = "02715"
-    offset = "1025"
-    @shift = Shift.new(key, offset)
+    date = "040895"
+    @shift = Shift.new(key, date)
   end
 
   def test_it_exists
@@ -26,36 +16,26 @@ class ShiftTest < Minitest::Test
     assert_instance_of Shift, shift
   end
 
-  # use this test if this is needed later
-  # def test_it_can_be_created_without_given_key_and_offset
-  #   shift = Shift.new
-  #   assert_instance_of Shift, shift
-  # end
-
   def test_it_has_attributes
     assert_equal "02715", @shift.key
-    assert_equal "1025", @shift.offset
+    assert_equal "040895", @shift.date
     assert_nil @shift.a_shift
     assert_nil @shift.b_shift
     assert_nil @shift.c_shift
     assert_nil @shift.d_shift
   end
 
-  def test_it_can_create_shift
-    @shift.create_shift
+  def test_it_can_create_offset
+    assert_equal "1025", @shift.create_offset
+  end
 
+  def test_it_can_create_shift
+
+    @shift.create_shift
     assert_equal 3, @shift.a_shift
     assert_equal 27, @shift.b_shift
     assert_equal 73, @shift.c_shift
     assert_equal 20, @shift.d_shift
   end
-#
-#   def test_it_can_create_shift
-#     @shift.create_shift
-#
-#     assert_equal 14, @shift.a_shift
-#     assert_equal 24, @shift.b_shift
-#     assert_equal 40, @shift.c_shift
-#     assert_equal 46, @shift.d_shift
-#   end
+
 end
