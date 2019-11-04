c8625da845549448ac46fc31f8f3ca489c823f67
Alison Vermeil <alison.minton@gmail.com>
2019-11-04 12:37:22 -0700

    Refactor tests for create_shift to include setting date and key IV and add all helper methods

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -27,6 +27,10 @@ class EnigmaTest < Minitest::Test
   def test_it_can_create_shift_with_key_and_date
 
     @enigma.create_shift("02715", "040895")
+
+    assert_equal "02715", @enigma.key
+    assert_equal "040895", @enigma.date
+
     assert_equal 3, @enigma.shift.a_shift
     assert_equal 27, @enigma.shift.b_shift
     assert_equal 73, @enigma.shift.c_shift
@@ -37,6 +41,9 @@ class EnigmaTest < Minitest::Test
     Date.expects(:today).returns(Date.new(2019,11,2))
 #offset should = "021119"
     @enigma.create_shift("02715")
+
+    assert_equal "02715", @enigma.key
+    assert_equal "021119", @enigma.date
 
     assert_equal 4, @enigma.shift.a_shift
     assert_equal 28, @enigma.shift.b_shift
@@ -49,10 +56,50 @@ class EnigmaTest < Minitest::Test
     Key.expects(:generate_number).returns("12345")
 
     @enigma.create_shift
+
+    assert_equal "12345", @enigma.key
+    assert_equal "021119", @enigma.date
+
     assert_equal 14, @enigma.shift.a_shift
     assert_equal 24, @enigma.shift.b_shift
     assert_equal 40, @enigma.shift.c_shift
     assert_equal 46, @enigma.shift.d_shift
+  end
+
+  def test_it_can_determine_shift_amount_by_character
+    @enigma.create_shift("02715", "040895")
+
+    assert_equal @enigma.shift.a_shift, @enigma.determine_shift_amount(0)
+    assert_equal @enigma.shift.b_shift, @enigma.determine_shift_amount(1)
+    assert_equal @enigma.shift.c_shift, @enigma.determine_shift_amount(2)
+    assert_equal @enigma.shift.d_shift, @enigma.determine_shift_amount(3)
+  end
+
+  def test_it_can_determine_if_character_is_in_alphabet
+    assert_equal true, @enigma.in_alphabet?("a")
+    assert_equal true, @enigma.in_alphabet?("h")
+    assert_equal true, @enigma.in_alphabet?(" ")
+    assert_equal true, @enigma.in_alphabet?("Z")
+    assert_equal false, @enigma.in_alphabet?("?")
+    assert_equal false, @enigma.in_alphabet?("!")
+  end
+
+  def test_it_can_find_alphabet_index_of_character
+    assert_equal 0, @enigma.alphabet_index("a")
+    assert_equal 12, @enigma.alphabet_index("M")
+    assert_equal 26, @enigma.alphabet_index(" ")
+  end
+
+  def test_it_can_shift_alphabet_a_given_amount
+    assert_equal 0, @enigma.alphabet_index("a")
+    rotated_alphabet = @enigma.shift_alphabet(3)
+    expected = rotated_alphabet.find_index("d")
+    assert_equal 0, expected
+
+    assert_equal 0, @enigma.alphabet_index("a")
+    rotated_alphabet = @enigma.shift_alphabet(-3)
+    expected = rotated_alphabet.find_index("y")
+    assert_equal 0, expected
   end
 
   def test_it_can_shift_characters
@@ -131,7 +178,7 @@ class EnigmaTest < Minitest::Test
     assert_equal expected, @enigma.encrypt("hello world")
   end
 
-#this next test might be totally bogus 
+#this next test might be totally bogus
   def test_it_can_decrypt_a_message_without_key_or_date
     Date.expects(:today).at_least_once.returns(Date.new(1995,8,4))
     Key.expects(:generate_number).at_least_once.returns("02715")
