cd7ce1a712a368baa0db6a7f3f93068baf5ef8c0
Alison Vermeil <alison.minton@gmail.com>
2019-11-04 12:05:43 -0700

    Add tests for creating shift w/ key only or no date or key plus encryption tests to match

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,6 +1,8 @@
 require './test/test_helper'
 require_relative '../lib/enigma'
 require_relative '../lib/shift'
+require_relative '../lib/offset'
+require_relative '../lib/key'
 
 class EnigmaTest < Minitest::Test
   def setup
@@ -25,11 +27,32 @@ class EnigmaTest < Minitest::Test
   def test_it_can_create_shift_with_key_and_date
 
     @enigma.create_shift("02715", "040895")
-
     assert_equal 3, @enigma.shift.a_shift
     assert_equal 27, @enigma.shift.b_shift
     assert_equal 73, @enigma.shift.c_shift
     assert_equal 20, @enigma.shift.d_shift
+  end
+
+  def test_it_can_create_shift_with_key_only
+    Date.expects(:today).returns(Date.new(2019,11,2))
+#offset should = "021119"
+    @enigma.create_shift("02715")
+
+    assert_equal 4, @enigma.shift.a_shift
+    assert_equal 28, @enigma.shift.b_shift
+    assert_equal 77, @enigma.shift.c_shift
+    assert_equal 16, @enigma.shift.d_shift
+  end
+
+  def test_it_can_create_shift_without_key_or_date
+    Date.expects(:today).returns(Date.new(2019,11,2))
+    Key.expects(:generate_number).returns("12345")
+
+    @enigma.create_shift
+    assert_equal 14, @enigma.shift.a_shift
+    assert_equal 24, @enigma.shift.b_shift
+    assert_equal 40, @enigma.shift.c_shift
+    assert_equal 46, @enigma.shift.d_shift
   end
 
   def test_it_can_shift_characters
@@ -67,5 +90,60 @@ class EnigmaTest < Minitest::Test
                 }
 
     assert_equal expected, @enigma.decrypt("keder ohulw", "02715", "040895")
+  end
+
+  def test_it_can_encrypt_a_message_with_key_only
+    Date.expects(:today).returns(Date.new(1995,8,4))
+
+    expected = {
+                encryption: "keder ohulw",
+                key: "02715",
+                date: "040895"
+                }
+
+    assert_equal expected, @enigma.encrypt("hello world", "02715")
+  end
+
+  def test_it_can_decrypt_a_message_with_key_only
+    Date.expects(:today).at_least_once.returns(Date.new(1995,8,4))
+
+    encrypted = @enigma.encrypt("hello world", "02715")
+
+    expected = {
+                decryption: "hello world",
+                key: "02715",
+                date: "040895"
+                }
+
+    assert_equal expected, @enigma.decrypt(encrypted[:encryption], "02715")
+  end
+
+  def test_it_can_encrypt_a_message_without_key_or_date
+    Date.expects(:today).returns(Date.new(1995,8,4))
+    Key.expects(:generate_number).returns("02715")
+
+    expected = {
+                encryption: "keder ohulw",
+                key: "02715",
+                date: "040895"
+                }
+
+    assert_equal expected, @enigma.encrypt("hello world")
+  end
+
+#this next test might be totally bogus 
+  def test_it_can_decrypt_a_message_without_key_or_date
+    Date.expects(:today).at_least_once.returns(Date.new(1995,8,4))
+    Key.expects(:generate_number).at_least_once.returns("02715")
+
+    encrypted = @enigma.encrypt("hello world")
+
+    expected = {
+                decryption: "hello world",
+                key: "02715",
+                date: "040895"
+                }
+
+    assert_equal expected, @enigma.decrypt(encrypted[:encryption])
   end
 end
