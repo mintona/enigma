4f2e0e707f6a21cbb91758a462e08d4333020934
Alison Vermeil <alison.minton@gmail.com>
2019-11-05 14:20:22 -0700

    Refactor decrypt to fix variable consistency

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -3,20 +3,17 @@ require_relative './shift'
 require_relative './key'
 require_relative './offset'
 require_relative './file_io'
-
 include FileIO
 
-ARGV == ['encrypted.txt', 'decrypted.txt', '', '']
+ARGV == ['encrypted.txt', 'decrypted.txt', 'key', 'date']
 ARGV[0] == 'encrypted.txt'
 ARGV[1] == 'decrypted.txt'
-ARGV[2] == ''
-ARGV[3] == ''
+ARGV[2] == 'key'
+ARGV[3] == 'date'
 
-encrypted_message = read_message(ARGV[0])
-
+message = read_message(ARGV[0])
 enigma = Enigma.new
-
-decryption = enigma.decrypt(encrypted_message, ARGV[2], ARGV[3])
+decryption = enigma.decrypt(message, ARGV[2], ARGV[3])
 
 write_message(ARGV[1], decryption[:decryption])
 
