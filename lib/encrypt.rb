7c721d8e168a7299e6249bad6b82bb26ffebc3a3
Alison Vermeil <alison.minton@gmail.com>
2019-11-04 19:02:04 -0700

    Update encrypt runner to include FileIO module

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -2,14 +2,16 @@ require_relative './enigma'
 require_relative './shift'
 require_relative './key'
 require_relative './offset'
+require_relative './file_io'
+
+include FileIO
 
 ARGV == ['message.txt', 'encrypted.txt']
 ARGV[0] == 'message.txt'
 ARGV[1] == 'encrypted.txt'
 
-message = File.open(ARGV[0], "r")
-message_text = message.readlines.join
-message_string = message_text.chomp
+message_string = read_message(ARGV[0])
+# message_string = FileIO.read_message(ARGV[0])
 
 key = Key.generate_number
 date = Offset.generate_date
@@ -18,7 +20,7 @@ enigma = Enigma.new
 
 encryption = enigma.encrypt(message_string, key, date)
 
-encrypted_text = File.open(ARGV[1], "w")
-encrypted_text = encrypted_text.write(encryption[:encryption])
+# FileIO.write_message(ARGV[1], encryption[:encryption])
+write_message(ARGV[1], encryption[:encryption])
 
 puts "Created '#{ARGV[1]}' with the key #{key} and date #{date}"
