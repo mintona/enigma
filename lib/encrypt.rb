585a104bf24f4189a7a68d1e3b00a7bf78ee2951
Alison Vermeil <alison.minton@gmail.com>
2019-11-05 12:10:57 -0700

    Refactor enigma.rb to not generate extra key/shift, tbd if will remain this way

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -11,14 +11,14 @@ message = File.open(ARGV[0], "r")
 message_text = message.readlines.join
 message_string = message_text.chomp
 
-key = Key.generate_number
-date = Offset.generate_date
+# key = Key.generate_number
+# date = Offset.generate_date
 
 enigma = Enigma.new
 
-encryption = enigma.encrypt(message_string, key, date)
-
+encryption = enigma.encrypt(message_string)
+# encryption = enigma.encrypt(message_string, key, date)
 encrypted_text = File.open(ARGV[1], "w")
 encrypted_text = encrypted_text.write(encryption[:encryption])
 
-puts "Created '#{ARGV[1]}' with the key #{key} and date #{date}"
+puts "Created '#{ARGV[1]}' with the key #{encryption[:key]} and date #{encryption[:date]}"
