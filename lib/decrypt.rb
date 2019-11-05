dccacce3ee1b506d805d90ad286e3e8889bb2e88
Alison Vermeil <alison.minton@gmail.com>
2019-11-04 19:01:46 -0700

    Update decrypt runner to include File IO module

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -2,6 +2,9 @@ require_relative './enigma'
 require_relative './shift'
 require_relative './key'
 require_relative './offset'
+require_relative './file_io'
+
+include FileIO
 
 ARGV == ['encrypted.txt', 'decrypted.txt', '', '']
 ARGV[0] == 'encrypted.txt'
@@ -9,15 +12,12 @@ ARGV[1] == 'decrypted.txt'
 ARGV[2] == ''
 ARGV[3] == ''
 
-encrypted_message = File.open(ARGV[0], "r")
-encrypted_text = encrypted_message.readlines.join
-encrypted_string = encrypted_text.chomp
+encrypted_message = read_message(ARGV[0])
 
 enigma = Enigma.new
 
-decryption = enigma.decrypt(encrypted_string, ARGV[2], ARGV[3])
+decryption = enigma.decrypt(encrypted_message, ARGV[2], ARGV[3])
 
-decrypted_text = File.open(ARGV[1], "w")
-decrypted_text = decrypted_text.write(decryption[:decryption])
+write_message(ARGV[1], decryption[:decryption])
 
 puts "Created '#{ARGV[1]}' with the key #{ARGV[2]} and date #{ARGV[3]}"
