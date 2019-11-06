8767c30c91db03e5b965983f15d76376848e7917
Alison Vermeil <alison.minton@gmail.com>
2019-11-05 20:27:22 -0700

    Change name of class from offset to date

--- lib/date.rb
+++ lib/date.rb
@@ -1,10 +1,10 @@
 require 'date'
 
-class Offset
+class Date
 
   def self.generate_date
     date = Date.today
     date.strftime("%d%m%y")
   end
-  
+
 end
--- test/date_test.rb
+++ test/date_test.rb
@@ -1,20 +1,20 @@
 require './test/test_helper'
 require_relative '../lib/key'
-require_relative '../lib/offset'
+require_relative '../lib/date'
 
 class OffsetTest < Minitest::Test
   def setup
-    @offset = Offset.new
+    @date = Date.new
   end
 
   def test_it_exists
-    assert_instance_of Offset, @offset
+    assert_instance_of Date, @date
   end
 
   def test_it_can_get_todays_date
     Date.expects(:today).returns(Date.new(2019,11,2))
 
-    assert_equal "021119", Offset.generate_date
+    assert_equal "021119", Date.generate_date
   end
 
 end
