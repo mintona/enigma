29de745a5335c0b3cdf623d52a1a1e4266a0fe86
Alison Vermeil <alison.minton@gmail.com>
2019-11-05 18:29:42 -0700

    Refactor shift to return an array instead of hash

--- lib/shift.rb
+++ lib/shift.rb
@@ -1,43 +1,34 @@
 require_relative './calculator'
 
 class Shift
-  include Calculator
+  # include Calculator
 
-  # attr_reader :key, :date, :shift_values
-  # :a_shift, :b_shift, :c_shift, :d_shift
-  #
-  # def initialize(key, date)
-  #   @key = key
-  #   @date = date
-  #   @shift_values = Hash.new({})
-  #   # @a_shift = nil
-  #   # @b_shift = nil
-  #   # @c_shift = nil
-  #   # @d_shift = nil
-  # end
+  def self.create_offset(date)
+    (date.to_i ** 2).to_s[-4..-1]
+  end
 
-  def self.create_offset(date = nil)
-    if date.nil?
-      date = Offset.generate_date
-    end
-    # require "pry"; binding.pry
-    # square(date.to_i).to_s[-4..-1]
-    (date.to_i * date.to_i).to_s[-4..-1]
-    #where do I test this square method from my calculator?
+  def self.split_key(key)
+    characters = key.each_char.map { |character| character}
+    pairs = characters.each_cons(2).map { |character| character }
+    pairs.map { |pair| (pair.first + pair.last).to_i }
   end
 
   def self.create_shift(key, date)
-    shift_values = Hash.new(0)
-    offset = create_offset(date)
-    shift_values[:a_shift] = key[0..1].to_i + offset[0].to_i
-    shift_values[:b_shift] = key[1..2].to_i + offset[1].to_i
-    shift_values[:c_shift] = key[2..3].to_i + offset[2].to_i
-    shift_values[:d_shift] = key[3..4].to_i + offset[3].to_i
-    # @a_shift = @key[0..1].to_i + offset[0].to_i
-    # @b_shift = @key[1..2].to_i + offset[1].to_i
-    # @c_shift = @key[2..3].to_i + offset[2].to_i
-    # @d_shift = @key[3..4].to_i + offset[3].to_i
-    shift_values
+    offset_array = create_offset(date).split("").map { |character| character.to_i }
+    key_array = split_key(key)
+
+    pairs = offset_array.zip(key_array)
+
+    pairs.map { |pair| pair.sum}
+
+    #
+    # shift_values = Hash.new(0)
+    # offset = create_offset(date)
+    # shift_values[:a_shift] = key[0..1].to_i + offset[0].to_i
+    # shift_values[:b_shift] = key[1..2].to_i + offset[1].to_i
+    # shift_values[:c_shift] = key[2..3].to_i + offset[2].to_i
+    # shift_values[:d_shift] = key[3..4].to_i + offset[3].to_i
+    # shift_values
   end
 
 end
--- test/shift_test.rb
+++ test/shift_test.rb
@@ -5,42 +5,38 @@ require_relative '../lib/shift'
 
 class ShiftTest < Minitest::Test
 
-  # def setup
-  #   key = "02715"
-  #   date = "040895"
-  #   @shift = Shift.new(key, date)
-  # end
+  def setup
+    @key = "02715"
+    @date = "040895"
+  end
 
   def test_it_exists
     shift = Shift.new#("12345", "021119")
     assert_instance_of Shift, shift
   end
 
-  # def test_it_has_attributes
-  #   assert_equal "02715", @shift.key
-  #   assert_equal "040895", @shift.date
-  #   assert_equal ({}), @shift.shift_values
-  #   # assert_nil @shift.a_shift
-  #   # assert_nil @shift.b_shift
-  #   # assert_nil @shift.c_shift
-  #   # assert_nil @shift.d_shift
+  def test_it_can_create_offset
+    assert_equal "1025", Shift.create_offset(@date)
+  end
+  #
+  # def test_it_can_create_offset_using_todays_date
+  #   Date.expects(:generate_date).returns(Date.new(1995,8,4))
+  #
+  #   assert_equal "1025", Shift.create_offset
   # end
 
-  def test_it_can_create_offset
-    assert_equal "1025", Shift.create_offset("040895")
-  end
-
-  def test_it_can_create_offset_using_todays_date
-    Date.expects(:generate_date).returns(Date.new(1995,8,4))
-
-    assert_equal "1025", Shift.create_offset
+  def test_it_can_split_key
+    assert_equal [2, 27, 71, 15], Shift.split_key(@key)
   end
 
   def test_it_can_create_shift
 
     Shift.create_shift("02715", "040895")
 
-    expected = {:a_shift=>3, :b_shift=>27, :c_shift=>73, :d_shift=>20}
+    # expected = {:a_shift=>3, :b_shift=>27, :c_shift=>73, :d_shift=>20}
+
+
+    expected = [3, 27, 73, 20]
     assert_equal expected, Shift.create_shift("02715", "040895")
 
     # assert_equal 3, @shift.shift_values[:a_shift]
